a2c407fc93e78f440403aed9fe7ec1e9f019761e
EddyVerbruggen <eddyverbruggen@gmail.com>
2013-03-22 10:53:33 +0100

    initial import

---
+++ js/camera.js
@@ -0,0 +1,78 @@
+"use strict";
+
+var clickedImage, sourceType, project = null;
+
+function captureImage() {
+  navigator.camera.getPicture(onSuccess, onFail, {
+    quality:50,
+    allowEdit:true, // ignored by Android
+    destinationType:Camera.DestinationType.DATA_URL, // base64
+    encodingType:Camera.EncodingType.JPEG,
+    sourceType:sourceType, // camera or photoroll, depending on user choice
+    targetWidth:398, // = double size for retina
+    targetHeight:266
+  });
+}
+
+function onSuccess(imageData) {
+  var theImage = "data:image/jpeg;base64," + imageData;
+
+//  $.mobile.loading('show');
+  // Set a delay so the spinner appears
+  setTimeout(function() {
+    doPost(
+      getServiceURL("/user/savephoto"),
+      {
+        'project' : project,
+        'photo' : imageData
+      },
+      true,
+      function(data) {
+        if (data.success) {
+          // change the image in the view
+          $(clickedImage).attr('src', theImage);
+          showAlert("Op de server opslaan is gelukt");
+//          $.mobile.loading('hide');
+        } else {
+          showAlert("Op de server opslaan is mislukt");
+//          $.mobile.loading('hide');
+        }
+      }
+    );
+  }, 250);
+}
+
+function onFail(message) {
+  if (message == "no camera available") {
+    navigator.notification.alert("Er is geen geschikte camera gevonden.", {}, "Probleempje..", "Sluiten");
+  }
+}
+
+function cameraIconClicked(projectId, theElement) {
+//  alert($(theElement).attr('src'));
+  project = projectId;
+  clickedImage = theElement;
+
+  if (isMobile()) {
+    navigator.notification.confirm(
+        'U kunt de foto later altijd veranderen.', // message
+        onConfirmNew, // callback to invoke with index of button pressed
+        'Foto toevoegen', // title
+        'Foto maken,Foto kiezen,Annuleren'  // buttonLabels
+    );
+  } else {
+    alert("Dit kan alleen op mobiel");
+  }
+}
+
+function onConfirmNew(buttonIndex) {
+  if (buttonIndex == 1) {    // index starts at 1
+    sourceType = Camera.PictureSourceType.CAMERA;
+    captureImage();
+  } else if (buttonIndex == 2) {
+    sourceType = Camera.PictureSourceType.PHOTOLIBRARY;
+    captureImage();
+  } else {
+    // cancelled
+  }
+}
---
+++ js/custom.js
@@ -0,0 +1,132 @@
+"use strict";
+
+// generic catch for errors
+window.onerror = function(message, file, line) {
+  showAlert('Error gevangen: ' + file + ':' + line + '\n' + message);
+  console.log('Error gevangen: ' + file + ':' + line + '\n' + message);
+};
+
+function isAndroid() {
+  return navigator.userAgent.toLowerCase().indexOf("android") > -1;
+}
+
+function isIOS() {
+  return navigator.userAgent.match(/(iPad|iPhone|iPod)/i);
+}
+
+function isMobile() {
+  return isIOS() || isAndroid();
+}
+
+function isMacBook() {
+  return window.location.hostname == 'tfg';
+}
+
+function getServiceURL(servicePath) {
+  if (isMobile()) {
+    return "http://mobiel.mytfg.nl:9000" + servicePath;
+  } else if (isMacBook()) {
+    return "http://mobiel.mytfg.nl:9000" + servicePath;
+//    return "http://localhost:9000" + servicePath;
+  } else {
+    return "http://www.thumbrater.com:9005" + servicePath;
+  }
+}
+
+function doPost(url, data, async, successFunction) {
+  invokeRemote('POST', url, data, async, successFunction);
+}
+
+function doGet(url, async, successFunction) {
+  invokeRemote('GET', url, null, async, successFunction);
+}
+
+function invokeRemote(method, url, data, async, successFunction) {
+  $.ajax({
+    type : method,
+    url  : url,
+    data : data,
+    dataType : 'json',
+    headers : {'X-username' : getLoginName(), 'X-token' : getTokenFromSession()},
+    async: async,
+    success : function(data) {
+                successFunction(data);
+              },
+    //TODO: Error Handling
+    error : function(XMLHttpRequest, textStatus, errorThrown) {
+              if (XMLHttpRequest.status = 401) {
+                $.mobile.changePage($("#loginPage"), {transition:"none"});
+              } else if (XMLHttpRequest.status == 0) {
+                showAlert("Server onbereikbaar " + errorThrown);
+              } else {
+                showAlert("Service failed: " + url + ", " + textStatus + "; " + errorThrown);
+              }
+            }
+  });
+}
+
+function getPhoto(person) {
+  if (person.mobileUser != null && person.mobileUser.photoContent != null) {
+    return "data:image/jpeg;base64,"+person.mobileUser.photoContent;
+  } else if (person.photoContent != null) {
+    return "data:image/jpeg;base64,"+person.photoContent;
+  } else if (person.gender == "MALE") {
+    return "img/pasfoto/male.jpeg";
+  } else if (person.gender == 'FEMALE') {
+    return "img/pasfoto/female.jpeg";
+  } else {
+    return "img/pasfoto/empty.gif";
+  }
+}
+
+//function savePersons(persons) {
+//  localStorage.setItem("persons", persons);
+//}
+
+//function loadPersons() {
+//  return localStorage.getItem("persons");
+//}
+
+//function addRequestsToCache(requests) {
+//  localStorage.setItem("requests", JSON.stringify(requests));
+//}
+
+//function loadRequests() {
+//  var storedRequests = localStorage.getItem("requests");
+//  if (storedRequests != "undefined") {
+//    return JSON.parse(storedRequests);
+//  } else {
+//    return null;
+//  }
+//}
+
+//function retrieveRequestsFromServer(successCallback) {
+//  doGet(
+//      getServiceURL("/request/list"),
+//      true,
+//      function(data) {
+//        if (data.success) {
+//          if (successCallback != null) {
+//            successCallback(data.output);
+//          }
+//        }
+//      }
+//  );
+//}
+
+function openWindow(pleaseTakeMeHere) {
+  // window.open uses the PG InAppBrowser API (http://docs.phonegap.com/en/2.5.0/cordova_inappbrowser_inappbrowser.md.html)
+  var ref = window.open(pleaseTakeMeHere, '_blank', 'location=no');
+  // Programmatically closing the InAppBrowser clears its history.
+  ref.addEventListener('exit', function() {
+    ref.close();
+  });
+}
+
+function showAlert(txt) {
+  if (isMobile()) {
+    navigator.notification.alert(txt, function(){}, "Melding");
+  } else {
+    alert(txt);
+  }
+}
